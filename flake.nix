a956252c583559d0022d3465a1135ce920e0d8cd
dspijkers <daan@spijke.rs>
2021-10-11 15:49:20 +0200

    Initial commit
    Adds poetry and nix support using poetry2nix, as well as general project
    structure.

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Thesis";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        myAppEnv = pkgs.poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
+        };
+
+        app = pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+        };
+
+        packageName = "thesis";
+      in {
+        packages.${packageName} = app;
+
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = myAppEnv.env.overrideAttrs (oldAttrs: {
+          buildInputs = [ pkgs.poetry ];
+        });
+      });
+}
