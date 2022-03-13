81fe20142b8574cb4fd4bb92a0e0b8bf0c0a3b5b
dspijkers <daan@spijke.rs>
2022-03-13 13:56:11 +0100

    code: add product_align to thesis/util

--- thesis/util.py
+++ thesis/util.py
@@ -1,18 +1,52 @@
 import numpy as np
 
 def choices(n, k):
-    if k == 1:
+    """
+    All k choose n combinations.
+    """
+    if (k < 1) or (k > len(n)):
+        raise ValueError("We must have 1 <= k <= len(n)")
+    elif k == 1:
         return n.reshape(n.shape[:1] + (1,) + n.shape[1:])
-    n_k = np.array([])
-    for i in range(n.shape[0]-k+1):
-        n_k_1 = choices(n[i+1:], k-1)
-        x = np.tile(n[i],
-                    n_k_1.shape[:1]
-                    + tuple(np.repeat(1, len(n_k_1.shape) -1)))
-        x_k = np.concatenate((x, n_k_1), axis=1)
-        if n_k.size == 0:
-            n_k = x_k
-        else:
-            n_k = np.concatenate((n_k, x_k))
+    else:
+        return np.concatenate(
+            [
+                np.concatenate(
+                    (
+                        n_k_1 := choices(n[i+1:], k-1),
+                        np.broadcast_to(
+                            n[i],
+                            (n_k_1.shape[0], 1) + n[i].shape
+                        )
+                    ),
+                    axis = 1
+                )
+                for i in range(n.shape[0]-k+1)
+            ]
+        )
 
-    return n_k
+def product_align(x, y):
+    """
+    Repeat x and y in such a way that concatenating x and y would return
+    their product.
+
+    Given x.shape = (x1, x2, ..., xn)
+    and y.shape = (y1, y2, ..., yn)
+    return xs and ys with shape (x1 * y1, x2, ...) (x1 * y1, y2, ...)
+    """
+    x_prod_dim = x.shape[0]
+    y_prod_dim = y.shape[0]
+    xs = np.broadcast_to(x, (y_prod_dim,) + (x.shape))
+    ys = np.broadcast_to(y, (x_prod_dim,) + (y.shape))
+
+    xs = np.reshape(
+        xs,
+        (x_prod_dim * y_prod_dim,) + (x.shape[1:]),
+        order='F'
+    )
+    ys = np.reshape(
+        ys,
+        (x_prod_dim * y_prod_dim,) + (y.shape[1:]),
+        order='C'
+    )
+    return xs, ys
