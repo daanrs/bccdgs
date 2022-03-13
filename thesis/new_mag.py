5281b0acd862080ae59f3285ab96eb274806465f
dspijkers <daan@spijke.rs>
2022-03-13 21:56:31 +0100

    wip score

--- thesis/new_mag.py
+++ thesis/new_mag.py
@@ -7,30 +7,35 @@ from thesis.util import (
 
 import numpy as np
 
-from numba import njit
-
 def gen_new_mag(g, lst, k):
     """
-    Generate all valid mags which can be created through changing a single
-    edge.
+    Generate the best scoring mag with k edges changed.
+
+    Note that this will not contain g.
     """
-    # all possible mark combinations
-    edges, marks = changes(g, k)
-    size = edges[-1, 0] + 1
-    b = big_array(g, size, edges, marks)
-    b = np.array([g for g in  b if valid_mag(g)])
-    s = np.argmax(np.array([score(g, lst) for g in b]))
-    return b[s]
+    size, edges, marks = changes(g, k)
+    gs = big_array(g, size, edges, marks)
+
+    #remove all copies of the original mag
+    gs = gs[gs != g]
+
+    # TODO: is this copy necessary?
+    # graph transitive closure
+    gst = gs.copy()
+    gst = np.array([dag_to_ancestral(to_directed(g)) for g in gst])
+
+    # check if there is a path x -> ... -> y and x <-* y
+    valids = ~ ((gst == 1) & (gs == 2)).any(axis=(1, 2))
+    gs = gs[valids]
+    gst = gst[valids]
+
+    scores = score(gst, gs, lst)
+    return gs[np.argmax(scores)]
 
 def changes(g, k):
-    # if keep_skeleton:
-        # # TODO: these edges are not ordered correctly
-        # marks = np.array([(1, 2), (2, 1), (2, 2)])
-        # edges = np.transpose(np.nonzero(g))
-    # else:
     marks = np.array([(0, 0), (1, 2), (2, 1), (2, 2)])
-    nodes = choices(np.arange(g.shape[0]), 2)
-    edges = add_edge_reverse(nodes)
+    edges = choices(np.arange(g.shape[0]), 2)
+    edges = add_edge_reverse(edges)
 
     marks, edges = product_align(
         choices(marks, k),
@@ -42,7 +47,8 @@ def changes(g, k):
     )
     edges = edges.reshape(-1, 3)
     marks = marks.reshape(-1)
-    return edges, marks
+    size = edges[-1, 0] + 1
+    return size, edges, marks
 
 def big_array(g, size, edges, marks):
     if edges.shape[0] != marks.shape[0]:
@@ -52,14 +58,3 @@ def big_array(g, size, edges, marks):
     b = np.tile(g, (size,) + tuple(np.repeat(1, len(g.shape))))
     b[edges[:, 0], edges[:, 1], edges[:, 2]] = marks
     return b
-
-@njit
-def valid_mag(mag):
-    """
-    Check if a mag has any (almost) directed cycles
-    """
-    # we get all possible directed paths from the mags transitive closure
-    ancestral = dag_to_ancestral(to_directed(mag.copy()))
-
-    # check if there is an adjacent path x -> ... -> y and x <-* y
-    return not ((ancestral == 1) & (mag == 2)).any()
--- thesis/score.py
+++ thesis/score.py
@@ -1,86 +1,62 @@
-from thesis.conversion import dag_to_ancestral, to_directed
-
 import numpy as np
-import numba
 
-@numba.njit
-def score(mag, lst):
+def score(gst, gs, sts):
     """
-    Score how well a mag fits to lst.
-
-    We use the score log(true_checks) + log(1 - false_checks), which is
-    the log-hikelihood of this happening, and so higher is better.
-    """
-    # we need the transitive closure to compute some statements, and it is
-    # much more efficient to calculate that once, then pass it on
-    mag_tc = dag_to_ancestral(to_directed(mag.copy()))
-
-    # we check which statments are correct
-    # since lst = [prob, statement] we index it [:, 1:]
-    smts = lst[:, 1:].astype(np.int8)
-    checks =  np.array([statement(mag_tc, mag, s) for s in smts])
-
-    # if checks is empty we return 0, since everything is true by default
-    if len(checks) == 0:
-        return 0
-    else:
-        # true_c = lst[checks][:, 0]
-        false_c = lst[~checks][:, 0]
-        # return np.sum(np.log(true_c)) + np.sum(np.log(1 - false_c))
-        # return np.sum(np.log(1 - false_c))
-        return np.sum(np.log(1 - false_c) - np.log(false_c))
-
-@numba.njit
-def statement(mag_tc, mag, statement):
-    """
-    Check whether a statement is correct.
-
+    TODO: this function is fucked up
     [x,x,z] = unconditional indep. : c = -3;    z < x # confounder/paths
     [x,y,x] = x/>y         && x/>S : c = -1
     [x,x,z] = no edge x-z          : c =  0;    x < z
     [x,y,z] = x=>y || x=>z || x=>S : c = +1
     [x,y,y] = x=>y         || x=>S : c = +2
 
-    [x,y,y] = x=>y                 : c = +3 (only Binfo/LoCI stage)
-    [x,y,x] = x/>y                 : c = -2 (only Binfo/LoCI stage)
-    [x,x,x] =                 x=>S : c = +4
-    [-,-,-] =                 x/>S : c = -4 (only Binfo/LoCI stage)
+    scoring function = np.sum(np.log(1 - false_c) - np.log(false_c))
+
+    statement = [c, score, x, y, z, ...]
     """
-    [c, x, y, z] = statement
+    if (
+        (sts[:, 0] < -3)
+        | (sts[:, 0] == -2)
+        | (sts[:, 0] > 2)
+    ).any():
+        raise ValueError("Statement type not in (-3, -1, 0, 1, 2)")
 
-    if c == -3:
-        return not (
-            ancestor(mag_tc, x, z)
-            or ancestor(mag_tc, z, x)
-            or cofounder(mag_tc, mag, x, z)
+    sts_cause_or = sts[sts[:, 0] == 2][:, 1:]
+    sts_cause = sts[sts[:, 0] == 1][:, 1:]
+    sts_edge = sts[sts[:, 0] == 0][:, 1:]
+    sts_noncause = sts[sts[:, 0] == -1][:, 1:]
+    sts_indep = sts[sts[:, 0] == -3][:, 1:]
+
+    return (
+        calc_score(
+            s_cause(gst, sts_cause_or[:, [1, 2]])
+            | s_cause(gst, sts_cause_or[:, [2, 3]]),
+            sts_cause_or[:, 0]
         )
-    elif c == -1:
-        return not ancestor(mag_tc, x, y)
-    elif c == 0:
-        return not edge(mag, x, z)
-    elif c == 1:
-        return ancestor(mag_tc, x, y) or ancestor(mag_tc, x, z)
-    elif c == 2:
-        return ancestor(mag_tc, x, y)
-    else:
-        raise ValueError('Statement type not in (-3, -1, 0, 1, 2)')
+        + calc_score(s_cause(gst, sts_cause[:, [1, 2]]), sts_cause[:, 0])
+        + calc_score(s_edge(gs, sts_edge[:, [1, 2]]), sts_edge[:, 0])
+        + calc_score(~s_cause(gst, sts_noncause[:, [1, 2]]), sts_noncause[:, 0])
+        + calc_score(s_indep(gst, gs, sts_indep[:, [1, 2]]), sts_indep[:, 0])
+    )
 
-@numba.njit
-def ancestor(mag_tc, x, y):
-    """Check if x is an ancestor of y."""
-    return (mag_tc[x, y] == 1)
+def calc_score(gs_sts, sts):
+    big_sts = np.broadcast_to(sts, gs_sts.shape)
+    false_sts = big_sts[~gs_sts]
+    return np.sum(np.log(1 - false_sts) - np.log(false_sts), axis=1)
 
-@numba.njit
-def edge(mag, x, y):
-    """Check if there is an edge between x and y."""
-    return (mag[x, y] != 0)
+def s_cause(gst, sts):
+    return gst[:, sts[:, 0], sts[:, 1]] == 1
 
-@numba.njit
-def cofounder(mag_tc, mag, x, y):
-    """Check if there is a cofounder between x and y."""
-    # check for x <-> y
-    if mag[x, y] == 2 and mag[y, x] == 2:
-        return True
-    # check for x <- ... <- z -> ... -> y
-    else:
-        return ((mag_tc[:, x] == 1) & (mag_tc[:, y] == 1)).any()
+def s_edge(gs, sts):
+    return gs[:, sts[:, 0], sts[:, 1]] != 0
+
+def s_indep(gst, gs, sts):
+    return ~ (
+        s_edge(gs, sts)
+        | s_cause(gst, sts)
+        | s_cause(gst, sts[:, ::-1])
+        | (
+            (gst[:, :, sts[:, 0]] == 1)
+            & (gst[:, :, sts[:, 1]] == 1)
+        ).any(axis=1)
+    )
+
